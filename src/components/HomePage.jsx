35ff877cdb2fd481f9092e44ea02ec8fba57172f
divine <divineosuuweb@gmail.com>
2024-08-08 11:22:21 +0200

    Adding a little marging to homepage title

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,7 +3,7 @@ import Table from './Table'
 
 export default function HomePage() {
   return <>
-    <div className='text-balance font-roboto text-3xl font-thin uppercase'>Demandes d'automatisations d'urbanisme</div>
+    <div className='text-balance font-roboto text-3xl font-thin uppercase mt-5'>Demandes d'automatisations d'urbanisme</div>
     <Table />
   </>
 }
